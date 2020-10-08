6bbbd6ea8d8c6e45fc27b66b28122b81da40e3d9
Isaac Barsoum <itbarsoum99@gmail.com>
2020-10-07 20:16:11 -0400

    first commit

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+let D = document
+			let mode = D.getElementById('mode')
+			const invert = _ => {
+					let sheet = D.createElement('style')
+					sheet.id = 'inverted'
+					sheet.innerHTML = `
+					body {
+						background-color: #eee;
+						color: black;
+					}
+					a {
+						color: #ff7600;
+					}
+					section {
+						background-color: #dcdcdc;
+					}
+					i {
+						color: navy !important;
+					}
+					`
+					D.head.appendChild(sheet)
+					localStorage.inverted = 'true'
+    }
+			if (localStorage.inverted) invert()
+		mode.onclick = _ => {
+			if (localStorage.inverted) {
+				let sheet = D.getElementById('inverted')
+				if (sheet) {
+					sheet.parentNode.removeChild(sheet)
+				}
+				localStorage.removeItem('inverted')
+			} else {
+				invert()
+			}
+		}
